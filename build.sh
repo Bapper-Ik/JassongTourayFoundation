dcedf0b67b75579fe87efe7902632f9a35d9815d
Yakubu Musa Yakubu <bappaik17@gmail.com>
2023-02-03 10:42:23 +0100

    editing build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 
 #build the project
 echo "Build the project..."
-python3.11.1 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo  "Make migrations"
-python3.11.1 manage.py makemigrations --noinput
-python3.11.1 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "collect static"
 python manage.py collectstatic  --noinput --clear
